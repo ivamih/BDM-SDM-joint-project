78cd77993544e5b576a866589b1114d3ef14d7f7
ivamih <ivamihajlovska@hotmail.com>
2020-06-14 16:24:39 +0200

    Spark transformations and add nodes to Neo4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import org.apache.log4j.LogManager;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.*;
+import org.neo4j.driver.internal.shaded.reactor.util.function.Tuple4;
 import org.neo4j.spark.*;
 import org.neo4j.spark.dataframe.Neo4jDataFrame;
 import org.neo4j.driver.*;
@@ -15,7 +17,9 @@ import org.neo4j.driver.Session;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Transaction;
 import org.neo4j.driver.TransactionWork;
+import scala.Tuple2;
 
+import static org.neo4j.driver.Values.ofFloat;
 import static org.neo4j.driver.Values.parameters;
 
 public class Main {
@@ -25,14 +29,26 @@ public class Main {
         // previousy create the node User and add constraint for unique Id in Neo4j
         String res = session.writeTransaction(new TransactionWork<String>()
         {
+            Result result;
             @Override
             public String execute( Transaction tx )
             {
-                Result result = tx.run( "LOAD CSV WITH HEADERS FROM 'file:///" + file + "' AS row\n" +
-                        "WITH toInteger(row.ID) AS id, row.Name AS name, row.CarOwner AS car_owner\n" +
-                        "MERGE (p:User {id: id})\n" +
-                        "SET p.name = name, p.car_owner = car_owner\n" +
-                        "RETURN count(p)");
+                if (file.equalsIgnoreCase("users.csv")){
+                    result = tx.run( "LOAD CSV WITH HEADERS FROM 'file:///" + file + "' AS row\n" +
+                            "WITH toInteger(row.ID) AS id, row.Name AS name, row.CarOwner AS car_owner\n" +
+                            "MERGE (p:User {id: id})\n" +
+                            "SET p.name = name, p.car_owner = car_owner\n" +
+                            "RETURN count(p)");
+                }
+                else if (file.equalsIgnoreCase("skopje_nodes.csv"))
+                {
+                    result = tx.run( "LOAD CSV FROM 'file:///" + file +"' AS row\n" +
+                            " WITH toInteger(row[0]) AS id, row[1] AS lat, row[2] AS lon\n" +
+                            " MERGE (p:Point {id: id})\n" +
+                            " SET p.lat = lat, p.long = lon, p.city ='Skopje'\n" +
+                            " RETURN count(p)");
+                }
+
                 return result.single().toString();
             }
         } );
@@ -45,17 +61,62 @@ public class Main {
         LogManager.getRootLogger().setLevel(Level.ERROR);
         LogManager.shutdown();
 
-        SparkConf conf = new SparkConf().setAppName("GO2").setMaster("local[*]");
-        JavaSparkContext ctx = new JavaSparkContext(conf);
+//        SparkConf conf = new SparkConf().setAppName("GO2").setMaster("local[*]");
+//        JavaSparkContext ctx = new JavaSparkContext(conf);
 
-        JavaRDD<String> users = ctx.textFile("src/main/resources/users.txt");
         driver = GraphDatabase.driver("bolt://localhost:7687",AuthTokens.basic("neo4j", "iva"));
         Session session = driver.session();
 
+        SparkSession spark_session = SparkSession.builder().master("local").appName("GO2").getOrCreate();
+
+//        UPLOAD USERS TO NEO4J
         System.out.println( executeTransaction(session, "users.csv") );
 
-//        String out = "";
-//        out += "The file has "+users.count()+" lines\n";
-//        System.out.println(out);
+//       CONVERT JSON TO CSV nodes ( Skopje )
+        Dataset<Row> dataset = spark_session.read().json("src/main/resources/skopje_graph.json");
+
+        dataset.printSchema();
+        Dataset<Row> d = dataset.select(functions.explode(dataset.col("e")).as("e"),dataset.col("la"), dataset.col("lo"));
+//        d.foreach(item -> {
+//            String[] s = item.get(0).toString().split(",");
+//            String s1 = s[0];
+//            String s2 = s[1];
+//            System.out.println(s1.substring(1,s1.length()) + " " + s2.substring(0,s2.length()-1) + " " + item.get(1) + " " + item.get(2));
+//        });
+
+        JavaRDD rdd = d.toJavaRDD();
+//        d.foreach(item -> {
+//            System.out.println(item);
+//            String[] s = item.get(0).toString().split(",");
+//            String s1 = s[0];
+//            String s2 = s[1];
+//            System.out.println(s1.substring(1,s1.length()) + " " + s2.substring(0,s2.length()-1) + " " + item.get(1) + " " + item.get(2));
+//        });
+        JavaRDD nodes = rdd.map(t ->
+        {
+            String a = t.toString().split(",")[2];
+            String b = t.toString().split(",")[3];
+            System.out.println(a);
+            System.out.println(b);
+            String[] id1 = a.split("\\.");
+            System.out.println(id1.length);
+            String[] id2 = b.substring(0,b.length()-1).split("\\.");
+            String id = id1[0] + id1[1] + id2[0] +id2[1];
+            return id+"," + a+","+b.substring(0,b.length()-1);
+        }).distinct();
+
+        nodes.saveAsTextFile("src/main/resources/skopje_nodes.csv");
+        System.out.println( executeTransaction(session, "skopje_nodes.csv") );
+
+
+
+
+
+//        CsvOutPutFormatPreprocessor<Row> csvOutPutFormatPreprocessor = new CsvOutPutFormatPreprocessor<Row>();
+//        Column[] flattened_column = csvOutPutFormatPreprocessor.flattenNestedStructure(d);
+//        d.select(flattened_column).write().mode(SaveMode.Overwrite).option("header", "true").format("csv").save("src/main/resources/belgrade");
+
+
+
     }
 }
