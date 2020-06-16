31e7bb1302291a06bb7dc2d8297449839b31ffb2
Gayane <gayanehvardanyan@gmail.com>
2020-06-16 15:52:49 +0200

    Generate near node

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,73 +1,76 @@
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.*;
-
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
-
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Session;
 
-
 public class Main {
 
-    static String HADOOP_COMMON_PATH = "C:\\Users\\Iva\\Desktop\\UPC\\BDM\\Project\\joint-project\\src\\main\\resources\\winutils";
+  static String HADOOP_COMMON_PATH =
+      "C:\\Users\\Iva\\Desktop\\UPC\\BDM\\Project\\joint-project\\src\\main\\resources\\winutils";
 
-    public static void main(String[] args) {
-        Driver driver;
-        Loader loader = new Loader();
-        Transformer transformer = new Transformer();
+  public static void main(String[] args) {
+    Driver driver;
+    Loader loader = new Loader();
+    Transformer transformer = new Transformer();
 
-        System.setProperty("hadoop.home.dir", HADOOP_COMMON_PATH);
-        LogManager.getRootLogger().setLevel(Level.ERROR);
-        LogManager.shutdown();
+    System.setProperty("hadoop.home.dir", HADOOP_COMMON_PATH);
+    LogManager.getRootLogger().setLevel(Level.ERROR);
+    LogManager.shutdown();
 
-//        SparkConf conf = new SparkConf().setAppName("GO2").setMaster("local[*]");
-//        JavaSparkContext ctx = new JavaSparkContext(conf);
+    //    SparkConf conf = new SparkConf().setAppName("GO2").setMaster("local[*]");
+    //        JavaSparkContext ctx = new JavaSparkContext(conf);
 
-        driver = GraphDatabase.driver("bolt://localhost:11002",AuthTokens.basic("neo4j", "iva"));
-        Session session = driver.session();
+    driver = GraphDatabase.driver("bolt://localhost:11002", AuthTokens.basic("neo4j", "iva"));
+    Session session = driver.session();
 
-        SparkSession spark_session = SparkSession.builder().master("local").appName("GO2").getOrCreate();
+    SparkSession spark_session =
+        SparkSession.builder().master("local").appName("GO2").getOrCreate();
+    Processor processor = new Processor(spark_session);
 
+    //        TRANSFORM USER ID
+    //        transformer.transformUsers(spark_session, "src/main/resources/Susers.csv");
+    //        transformer.transformUsers(spark_session, "src/main/resources/Busers.csv");////
+    // CONVERT JSON TO CSV nodes ( Skopje )
+    //        JavaRDD rdd = transformer.transformNodes(spark_session,
+    // "src/main/resources/skopje_graph.json");
+    //
+    ////      TRANSFORM EDGES BETWEEN SKOPJE NODES
+    //        transformer.transformEdges(rdd,"src/main/resources/skopje_graph.json");
+    //
+    ////       CONVERT JSON TO CSV nodes ( Belgrade )
+    //        JavaRDD rddB = transformer.transformNodes(spark_session,
+    // "src/main/resources/belgrade_graph.json");
+    //
+    ////      TRANSFORM EDGES BETWEEN BELGRADE NODES
+    //        transformer.transformEdges(rddB,"src/main/resources/belgrade_graph.json");
 
-//        TRANSFORM USER ID
-//        transformer.transformUsers(spark_session, "src/main/resources/Susers.csv");
-//        transformer.transformUsers(spark_session, "src/main/resources/Busers.csv");
+    //        CREATE SKOPJE PATH NODES
+    //        transformer.transformPaths(ctx, "src/main/resources/skopje_paths_no_time.csv");
+    //
+    ////        CREATE BELGRADE PATH NODES
+    //        transformer.transformPaths(ctx, "src/main/resources/belgrade_paths_no_time.csv");
 
-////       CONVERT JSON TO CSV nodes ( Skopje )
-//        JavaRDD rdd = transformer.transformNodes(spark_session, "src/main/resources/skopje_graph.json");
-//
-////      TRANSFORM EDGES BETWEEN SKOPJE NODES
-//        transformer.transformEdges(rdd,"src/main/resources/skopje_graph.json");
-//
-////       CONVERT JSON TO CSV nodes ( Belgrade )
-//        JavaRDD rddB = transformer.transformNodes(spark_session, "src/main/resources/belgrade_graph.json");
-//
-////      TRANSFORM EDGES BETWEEN BELGRADE NODES
-//        transformer.transformEdges(rddB,"src/main/resources/belgrade_graph.json");
+    //        Infer BELGRADE near Nodes
+    //    processor.inferNearPoints(
+    //        "src/main/resources/belgrade_nodes.csv", "src/main/resources/belgrade_near_nodes.csv",
+    // 0.5);
+    //        Infer SKOPJE near Nodes
+    //    processor.inferNearPoints(
+    //        "src/main/resources/skopje_nodes.csv", "src/main/resources/skopje_near_nodes.csv",
+    // 0.5);
 
-//        CREATE SKOPJE PATH NODES
-//        transformer.transformPaths(ctx, "src/main/resources/skopje_paths_no_time.csv");
-//
-////        CREATE BELGRADE PATH NODES
-//        transformer.transformPaths(ctx, "src/main/resources/belgrade_paths_no_time.csv");
+    //        UPLOAD to NEO4J
+    //            System.out.println( loader.executeTransaction(session, "skopje_users.csv"));
+    //        System.out.println( loader.executeTransaction(session, "belgrade_users.csv"));
+    //        System.out.println( loader.executeTransaction(session, "skopje_nodes.csv") );
+    //        System.out.println( loader.executeTransaction(session, "skopje_ways.csv") );
+    //        System.out.println( loader.executeTransaction(session, "belgrade_nodes.csv") );
+    //        System.out.println( loader.executeTransaction(session, "belgrade_ways.csv") );
+    //        System.out.println( loader.executeTransaction(session, "skopje_paths_nodes.csv") );
+    //        System.out.println( loader.executeTransaction(session, "belgrade_paths_nodes.csv") );
 
-
-//        UPLOAD to NEO4J
-        System.out.println( loader.executeTransaction(session, "skopje_users.csv"));
-        System.out.println( loader.executeTransaction(session, "belgrade_users.csv"));
-//        System.out.println( loader.executeTransaction(session, "skopje_nodes.csv") );
-//        System.out.println( loader.executeTransaction(session, "skopje_ways.csv") );
-//        System.out.println( loader.executeTransaction(session, "belgrade_nodes.csv") );
-//        System.out.println( loader.executeTransaction(session, "belgrade_ways.csv") );
-//        System.out.println( loader.executeTransaction(session, "skopje_paths_nodes.csv") );
-//        System.out.println( loader.executeTransaction(session, "belgrade_paths_nodes.csv") );
-
-    }
+  }
 }
---
+++ src/main/java/Processor.java
@@ -0,0 +1,55 @@
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.col;
+
+public class Processor {
+  private SparkSession sparkSession;
+
+  Processor(SparkSession sparkSession) {
+    this.sparkSession = sparkSession;
+  }
+
+  void inferNearPoints(String inputPath, String outputPath, double distanceInKilometers) {
+    Dataset<Row> points = sparkSession.read().csv(inputPath).cache();
+    Dataset<Row> joinedPoints =
+        points
+            .as("t")
+            .crossJoin(points.as("o"))
+            .select(
+                col("t._c0").as("node_1"),
+                col("o._c0").as("node_2"),
+                col("t._c1").cast("double").as("lat_1"),
+                col("o._c1").cast("double").as("lat_2"),
+                col("t._c2").cast("double").as("lon_1"),
+                col("o._c2").cast("double").as("lon_2"));
+    FilterFunction<Row> distanceFilter =
+        (FilterFunction<Row>)
+            row -> {
+              double lat1 = row.getDouble(row.fieldIndex("lat_1"));
+              double lat2 = row.getDouble(row.fieldIndex("lat_2"));
+              double lon1 = row.getDouble(row.fieldIndex("lon_1"));
+              double lon2 = row.getDouble(row.fieldIndex("lon_2"));
+              if ((lat1 == lat2) && (lon1 == lon2)) {
+                return true;
+              } else {
+                double theta = lon1 - lon2;
+                double dist =
+                    Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
+                        + Math.cos(Math.toRadians(lat1))
+                            * Math.cos(Math.toRadians(lat2))
+                            * Math.cos(Math.toRadians(theta));
+                dist = Math.acos(dist);
+                dist = Math.toDegrees(dist);
+                dist = dist * 60 * 1.1515;
+                dist = dist * 1.609344;
+                return dist <= distanceInKilometers;
+              }
+            };
+    Dataset<Row> nearPoints = joinedPoints.filter(distanceFilter);
+    Dataset<Row> nearNodes = nearPoints.select(nearPoints.col("node_1"), nearPoints.col("node_2"));
+    nearNodes.write().csv(outputPath);
+  }
+}
